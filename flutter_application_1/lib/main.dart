2a72b5ca7f1799dd0119a6cdfe67180c9af581da
lazychach <charlotte.1006.blouin@gmail.com>
2024-02-07 10:27:06 +0100

    bref

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -41,18 +41,21 @@ class MyHomePage extends StatelessWidget {
     var pair = appState.current;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('a random idea'),
-          BigCard(pair: pair),      
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext(); ;
-            },
-            child: Text('Next'),
-          ),
-
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('a random idea'),
+            BigCard(pair: pair),      
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext(); ;
+              },
+              child: Text('Next'),
+            ),
+        
+          ],
+        ),
       ),
     );
   }
@@ -77,8 +80,11 @@ class BigCard extends StatelessWidget {
       color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Text(pair.asLowerCase, style: style),
-
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
       ),
     );
   }
