e7c8fc8eeaf8eeb6d31a716fdad3c3171e4c4939
tsowden <tsowden.sup@gmail.com>
2024-02-16 09:59:51 +0100

    starting_project

---
+++ TP_LeBonCoin/app/src/main/java/fr/imt/ne/leboncoin/AdAddActivity.java
@@ -0,0 +1,14 @@
+package fr.imt.ne.leboncoin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ad_add);
+    }
+}
