fae69c8850c8e384fddd99ec66272405ed6814f1
tsowden <tsowden.sup@gmail.com>
2024-02-21 08:26:05 +0100

    test

--- TP_LeBonCoin/app/src/main/java/fr/imt/ne/leboncoin/MainActivity.java
+++ TP_LeBonCoin/app/src/main/java/fr/imt/ne/leboncoin/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    //Test blabla
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
