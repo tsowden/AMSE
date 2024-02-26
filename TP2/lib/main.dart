f6456ddff5b7e7544ceafcb4f22801296fc5a7ab
lazychach <charlotte.1006.blouin@gmail.com>
2024-02-26 09:01:00 +0100

    debug main

--- TP2/lib/main.dart
+++ TP2/lib/main.dart
@@ -6,10 +6,11 @@ import 'screens/exo2b.dart';
 
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,34 +18,35 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('TP2'),
+        title: const Text('TP2'),
       ),
       body: ListView(
         children: <Widget>[
           ListTile(
-            title: Text('Exercice 1'),
+            title: const Text('Exercice 1'),
             onTap: () {
               Navigator.of(context).push(MaterialPageRoute(builder: (context) => Exo1Screen()));
             },
           ),
           ListTile(
-            title: Text('Exercice 2'),
+            title: const Text('Exercice 2'),
             onTap: () {
               Navigator.of(context).push(MaterialPageRoute(builder: (context) => Exo2Screen()));
             },
           ),
           ListTile(
-            title: Text('Exercice 2b'),
+            title: const Text('Exercice 2b'),
             onTap: () {
               Navigator.of(context).push(MaterialPageRoute(builder: (context) => Exo2bScreen()));
             },
