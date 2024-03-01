493f488472439cf435090357eeff437591906d15
tsowden <tsowden.sup@gmail.com>
2024-03-01 13:59:38 +0100

    grille + debut dark theme + bouton start/stop

--- TP2/lib/screens/exo7.dart
+++ TP2/lib/screens/exo7.dart
@@ -1,12 +1,51 @@
 import 'dart:math' as math;
 import 'package:flutter/material.dart';
+import '../theme/theme.dart';
 
 math.Random random = math.Random();
+
+class Tile {
+  final int? id;
+  final Alignment alignment;
+
+  Tile(this.id, this.alignment);
+}
+
+class ImageTile extends StatelessWidget {
+  final String imageUrl;
+  final Tile tile;
+
+  const ImageTile({super.key, 
+    required this.imageUrl,
+    required this.tile,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FittedBox(
+      fit: BoxFit.fill,
+      child: ClipRect(
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.white, width: 3),
+          ),
+          child: Align(
+            alignment: tile.alignment,
+            widthFactor: 1 / 3,
+            heightFactor: 1 / 3,
+            child: tile.id != null ? Image.network(imageUrl) : const SizedBox.shrink(),
+          ),
+        ),
+      ),
+    );
+  }
+}
 
 class Exo7Screen extends StatefulWidget {
   const Exo7Screen({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _Exo7ScreenState createState() => _Exo7ScreenState();
 }
 
@@ -14,24 +53,28 @@ class _Exo7ScreenState extends State<Exo7Screen> {
   late List<Tile> tiles;
   final String imageUrl = 'https://picsum.photos/512';
   int _score = 0;
-  bool _showOriginalImage = false;
-  bool _showTchoupi = false;
+  bool _gameStarted = false;
+  bool _showOriginalImage = false; // Now will be used
+  bool _showTchoupi = false; // Now will be used
+  int _gridSize = 3;
   List<String> phrases = [
-  "Pas très bien joué, dommage.",
-  "D'ici 200 ou 300 coups, tu vas y arriver.",
-  "J'aurais pas fait ça personnellement.",
-  "Aie aie aie",
-  "Concentre toi un peu !",
-  "T'essayes, c'est le plus important.",
-  "...",
-  "Bien joué !! ^^'",
+    "Pas très bien joué, dommage.",
+    "D'ici 200 ou 300 coups, tu vas y arriver.",
+    "J'aurais pas fait ça personnellement.",
+    "Aie aie aie",
+    "Concentre toi un peu !",
+    "T'essayes, c'est le plus important.",
+    "...",
+    "Bien joué aha ^^'",
+    "C'est ton chat qui joue ?",
+    "Anthony Fleury jouait mieux...",
+    "Faut arrêter de cliquer aléatoirement.",
+    "C'est long, non ?",
+    "Je m'ennuie.",
+    "Je suis un pingouin. Tu es un manchot."
+  ];
 
-
-
-
-];
-
-  String _currentPhrase = "";
+  String _currentPhrase = ""; // Now will be used
 
   @override
   void initState() {
@@ -41,28 +84,60 @@ class _Exo7ScreenState extends State<Exo7Screen> {
     _selectRandomPhrase();
   }
 
+  void _startGame() {
+    setState(() {
+      _gameStarted = true;
+      _initializeTiles();
+      _score = 0; // Reset the score
+    });
+  }
+
+  void _stopGame() {
+    setState(() {
+      _gameStarted = false;
+      _score = 0;
+    });
+  }
+
+  void _increaseGridSize() {
+    if (_gridSize < 5) {
+      setState(() {
+        _gridSize++;
+        _initializeTiles(); // Re-initialize the tiles for the new grid size
+      });
+    }
+  }
+
+  void _decreaseGridSize() {
+    if (_gridSize > 3) {
+      setState(() {
+        _gridSize--;
+        _initializeTiles(); // Re-initialize the tiles for the new grid size
+      });
+    }
+  }
+
   void _selectRandomPhrase() {
     setState(() {
       _currentPhrase = phrases[random.nextInt(phrases.length)];
     });
   }
+
   void _initializeTiles() {
-    List<Alignment> alignments = [
-      Alignment(-1.0, -1.0), Alignment(0.0, -1.0), Alignment(1.0, -1.0),
-      Alignment(-1.0, 0.0), Alignment(0.0, 0.0), Alignment(1.0, 0.0),
-      Alignment(-1.0, 1.0), Alignment(0.0, 1.0), Alignment(1.0, 1.0),
-    ];
+    List<Alignment> alignments = createAlignments(_gridSize);
+    int tileCount = _gridSize * _gridSize;
 
-    tiles = List<Tile>.generate(9, (index) => Tile(index, alignments[index]));
-    tiles[8] = Tile(null, alignments[8]);
+    tiles = List<Tile>.generate(tileCount, (index) => Tile(index, alignments[index]));
+    tiles[tileCount - 1] = Tile(null, alignments[tileCount - 1]);
 
-    for (int i = 0; i < 100; i++) {
-      int randomIndex1 = random.nextInt(9);
-      int randomIndex2 = random.nextInt(9);
-
-      Tile temp = tiles[randomIndex1];
-      tiles[randomIndex1] = tiles[randomIndex2];
-      tiles[randomIndex2] = temp;
+    for (int i = 0; i < tileCount * 5; i++) {
+      int randomIndex1 = random.nextInt(tileCount);
+      int randomIndex2 = random.nextInt(tileCount);
+      if (randomIndex1 != randomIndex2) {
+        Tile temp = tiles[randomIndex1];
+        tiles[randomIndex1] = tiles[randomIndex2];
+        tiles[randomIndex2] = temp;
+      }
     }
   }
 
@@ -83,14 +158,15 @@ class _Exo7ScreenState extends State<Exo7Screen> {
   }
 
   List<int> getAdjacentIndices(int index) {
-    int row = index ~/ 3;
-    int col = index % 3;
+    int gridSize = _gridSize;
+    int row = index ~/ gridSize;
+    int col = index % gridSize;
     List<int> adjacentIndices = [];
 
-    if (row > 0) adjacentIndices.add(index - 3);
-    if (row < 2) adjacentIndices.add(index + 3);
+    if (row > 0) adjacentIndices.add(index - gridSize);
+    if (row < gridSize - 1) adjacentIndices.add(index + gridSize);
     if (col > 0) adjacentIndices.add(index - 1);
-    if (col < 2) adjacentIndices.add(index + 1);
+    if (col < gridSize - 1) adjacentIndices.add(index + 1);
 
     return adjacentIndices;
   }
@@ -105,18 +181,18 @@ class _Exo7ScreenState extends State<Exo7Screen> {
   }
 
   void _showVictoryDialog() {
-    showDialog(
+    showDialog<void>(
       context: context,
-      builder: (BuildContext context) {
+      builder: (context) {
         return AlertDialog(
-          title: Text("Félicitations !"),
-          content: Text("Vous avez réussi à reconstituer l'image !"),
+          title: const Text("Félicitations !"),
+          content: const Text("Vous avez réussi à reconstituer l'image !"),
           actions: <Widget>[
             TextButton(
-              child: Text("OK"),
+              child: const Text("OK"),
               onPressed: () {
-                Navigator.of(context).pop();
-                _initializeTiles();
+                Navigator.of(context).pop(); // Close the dialog
+                _initializeTiles(); // Re-initialize the game state
               },
             ),
           ],
@@ -125,30 +201,43 @@ class _Exo7ScreenState extends State<Exo7Screen> {
     );
   }
 
+  List<Alignment> createAlignments(int gridSize) {
+    List<Alignment> alignments = [];
+    for (int row = 0; row < gridSize; row++) {
+      for (int col = 0; col < gridSize; col++) {
+        double x = (col * 2 / (gridSize - 1)) - 1;
+        double y = (row * 2 / (gridSize - 1)) - 1;
+        alignments.add(Alignment(x, y));
+      }
+    }
+    return alignments;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Theme(
+    data: AppTheme.darkTheme, // Use the dark theme defined in your theme.dart file
+    child: Scaffold(
       appBar: AppBar(
         title: const Text('Exercice 7'),
       ),
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text('Score: $_score', style: TextStyle(fontSize: 24)),
-          ),
           Expanded(
             child: GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: _gridSize,
               ),
-              itemCount: tiles.length,
+              itemCount: _gridSize * _gridSize,
               itemBuilder: (context, index) {
                 return GestureDetector(
                   onTap: () {
-                    _swapTiles(index);
+                    if (_gameStarted) {
+                      _swapTiles(index);
+                    }
                   },
                   child: ImageTile(
+                    key: ValueKey(index), // Ajout pour optimiser la performance de rendu
                     imageUrl: imageUrl,
                     tile: tiles[index],
                   ),
@@ -156,21 +245,58 @@ class _Exo7ScreenState extends State<Exo7Screen> {
               },
             ),
           ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text('Déplacements: $_score', style: const TextStyle(fontSize: 20)),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: ElevatedButton(
+              onPressed: _gameStarted ? _stopGame : _startGame,
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                  (states) {
+                    if (_gameStarted) {
+                      return Colors.red; // Stop button color
+                    } else {
+                      return Colors.green; // Start button color
+                    }
+                  },
+                ),
+                foregroundColor: MaterialStateProperty.all<Color>(Colors.white), // Text color
+              ),
+              child: Text(_gameStarted ? 'Stopper la partie' : 'Lancer la partie'),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                icon: const Icon(Icons.arrow_right_alt), 
+                onPressed: !_gameStarted ? _decreaseGridSize : null,
+              ),
+              Text('Taille de la grille : $_gridSize'),
+              IconButton(
+                icon: const Icon(Icons.zoom_in), 
+                onPressed: !_gameStarted ? _increaseGridSize : null,
+              ),
+            ],
+          ),
           ElevatedButton(
             onPressed: () {
               setState(() {
                 _showTchoupi = !_showTchoupi;
               });
             },
-            child: Text("Afficher T'choupi"),
+            child: Text(_showTchoupi ? "Cacher T'choupi" : "Afficher T'choupi"),
           ),
           if (_showTchoupi)
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-                DialogueBubble(text: _currentPhrase),
-                SizedBox(width: 10), 
+                // DialogueBubble est maintenant à gauche de l'image T'choupi
+                DialogueBubble(key: const ValueKey('DialogueBubble'), text: _currentPhrase),
+                const SizedBox(width: 10), // Espacement entre le texte et l'image
                 Image.asset(
                   'assets/imgs/Tchoup.png',
                   width: 80,
@@ -178,10 +304,6 @@ class _Exo7ScreenState extends State<Exo7Screen> {
                 ),
               ],
             ),
-
-
-
-
           ElevatedButton(
             onPressed: () {
               setState(() {
@@ -208,14 +330,16 @@ class _Exo7ScreenState extends State<Exo7Screen> {
             ),
         ],
       ),
-    );
+    ));
   }
-}
+
+
+    }
 
 class DialogueBubble extends StatelessWidget {
   final String text;
 
-  DialogueBubble({required this.text});
+  const DialogueBubble({super.key, required this.text});
 
   @override
   Widget build(BuildContext context) {
@@ -229,50 +353,13 @@ class DialogueBubble extends StatelessWidget {
             color: Colors.black.withOpacity(0.05),
             spreadRadius: 1,
             blurRadius: 10,
-            offset: Offset(0, 3),
+            offset: const Offset(0, 3),
           ),
         ],
       ),
       child: Text(
         text,
-        style: TextStyle(color: Colors.black),
-      ),
-    );
-  }
-}
-
-class Tile {
-  final int? id;
-  final Alignment alignment;
-
-  Tile(this.id, this.alignment);
-}
-
-class ImageTile extends StatelessWidget {
-  final String imageUrl;
-  final Tile tile;
-
-  ImageTile({
-    required this.imageUrl,
-    required this.tile,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return FittedBox(
-      fit: BoxFit.fill,
-      child: ClipRect(
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.white, width: 3),
-          ),
-          child: Align(
-            alignment: tile.alignment,
-            widthFactor: 1 / 3,
-            heightFactor: 1 / 3,
-            child: tile.id != null ? Image.network(imageUrl) : SizedBox.shrink(),
-          ),
-        ),
+        style: const TextStyle(color: Colors.black),
       ),
     );
   }
--- TP2/lib/theme/theme.dart
+++ TP2/lib/theme/theme.dart
@@ -1,55 +1,36 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static ThemeData lightTheme = ThemeData(
-    // Propriétés du thème clair
-    secondaryHeaderColor: const Color.fromARGB(255, 246, 225, 36),
-    primaryColor: const Color.fromRGBO(205, 16, 132, 1),
-    backgroundColor: const Color.fromRGBO(126, 168, 212, 1),
-
-//Text Theme
-    textTheme: const TextTheme(
-      headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold, fontFamily:'Break'),
-      headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-      bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'KenyanCoffee'),
-      button: TextStyle(
-        color: Color.fromRGBO(205, 16, 132, 1), 
-        fontSize: 24.0,
-        fontFamily: 'Lato',
-        fontWeight: FontWeight.bold
+  static ThemeData get darkTheme {
+    return ThemeData(
+      brightness: Brightness.dark,
+      primaryColor: const Color.fromARGB(255, 24, 27, 39), // Dark background color
+      scaffoldBackgroundColor: const Color.fromARGB(255, 24, 27, 39),
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Color.fromARGB(0, 24, 27, 39), // Transparent AppBar
+        titleTextStyle: TextStyle(color: Color.fromARGB(255, 188, 162, 199), fontSize: 20),
+        elevation: 0, // No shadow
       ),
-    ),
-
-//Button Theme
-    buttonTheme: const ButtonThemeData(
-      buttonColor: Color.fromARGB(255, 246, 225, 36),
-    ),
-
-//App Bar
-    appBarTheme: const AppBarTheme(
-      color: Colors.blue,
-      elevation: 0.0,
-      iconTheme: IconThemeData(color: Colors.white),
-    ),
-
-    //Champs de saisie
-    inputDecorationTheme: const InputDecorationTheme(
-      border: OutlineInputBorder(),
-      labelStyle: TextStyle(color:Color.fromRGBO(205, 16, 132, 1)),
-    ),
-
-    iconTheme: IconThemeData(
-      color: const Color.fromARGB(255, 255, 255, 255),
-      size: 24.0,
-    ),
-
-
-  );
-
-  static ThemeData darkTheme = ThemeData(
-    // Définissez ici les propriétés du thème sombre
-    primaryColor: Colors.indigo,
-    secondaryHeaderColor: Colors.amber,
-    // ...
-  );
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          primary: const Color.fromARGB(201, 149, 0, 255), // Vibrant purple button
+          onPrimary: const Color.fromARGB(255, 229, 213, 236), // Text color
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+          padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 15.0),
+        ),
+      ),
+      textTheme: const TextTheme(
+        headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 158, 20, 216)), // For large titles
+        headline6: TextStyle(fontSize: 36.0, color: Color.fromARGB(255, 188, 162, 199)), // For subtitles
+        bodyText2: TextStyle(fontSize: 14.0, color: Colors.white), // For body text
+      ),
+      buttonTheme: const ButtonThemeData(
+        buttonColor: Color.fromARGB(201, 149, 0, 255), // Buttons
+      ),
+      iconTheme: const IconThemeData(
+        color: Color.fromARGB(255, 188, 162, 199),
+      ),
+      // Add additional theming as needed...
+    );
+  }
 }
